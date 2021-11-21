5c380693376e9955e9052ad5fa2c483070cea3e9
Guilherme-Ferreti <guiferretis@gmail.com>
2021-11-21 16:37:41 -0300

    Gravatar images

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -11,7 +11,7 @@ class IdeaController extends Controller
     public function index()
     {
         return view('idea.index', [
-            'ideas' => Idea::simplePaginate(Idea::PAGINATION_COUNT),
+            'ideas' => Idea::with('user')->simplePaginate(Idea::PAGINATION_COUNT),
         ]);
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,18 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatar(): string
+    {
+        $firstCharacter = $this->email[0];
+
+        $integerToUse = is_numeric($firstCharacter)
+            ? $integerToUse = ord($firstCharacter) - 21
+            : $integerToUse = ord(strtolower($firstCharacter)) - 96;
+
+        return 'https://www.gravatar.com/avatar/'
+                . md5($this->email)
+                . '?size=200'
+                .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-' . $integerToUse . '.png';
+    }
 }
