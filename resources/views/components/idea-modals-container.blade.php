2ef33975b2b8c72c7b400466aae797b19887a500
Guilherme-Ferreti <guiferretis@gmail.com>
2021-12-02 10:09:53 -0300

    Refactored modals to blade component

---
+++ resources/views/components/idea-modals-container.blade.php
@@ -0,0 +1,17 @@
+@push('modals')
+    @can('update', $idea)
+        <livewire:edit-idea :idea="$idea" />
+    @endcan
+
+    @can('delete', $idea)
+        <livewire:delete-idea :idea="$idea" />   
+    @endcan
+
+    @auth
+        <livewire:mark-idea-as-spam :idea="$idea" />
+    @endauth
+
+    @can('markAsNotSpam', \App\Models\Idea::class)
+        <livewire:mark-idea-as-not-spam :idea="$idea" />
+    @endcan
+@endpush
